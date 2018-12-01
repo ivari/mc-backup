61bb400ec78ee50cc2bf20dc5150a9d72558ef09
steve <noreply@balticminecraft.eu>
2018-12-01 05:55:01 +0000

    backup state on Sat Dec  1 05:55:01 UTC 2018

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/steve/minecraft/server
+
+git add competition*
+git add plugins
+git add .json
+
+git add .gitignore
+git add backup.sh
+
+git commit -m "backup state on $(date)"
+git push
+
